9d5f2835ed8c906b02881557d414ee7d607a243e
victor.amigos111@gmail.com <Victor Fernandes>
2026-04-25 19:31:12 -0300

    atividade finalizada

--- public/script.js
+++ public/script.js
@@ -1,26 +1,48 @@
-let nome, despesas, renda;
+let nome, n, despesas, renda, i, soma = 0, sobra = 0;
 
 nome = prompt("Digite seu nome:");
 
 do {
     renda = Number(
         prompt("Digite sua renda mensal:")
-);
+    );
 } while (isNaN(renda));
 
+do {
+    n = Number(
+        prompt("Quantas despesas deseja informar? ")
+    );
+} while (isNaN(n));
 
-despesas = Number(
-    prompt("Quantas despesas deseja informar?")
-);
-
-if (despesas < 1) {
-    despesas = 1;
+if (n < 1) {
+    n = 1;
 }
 
-if (despesas > 5) {
-    despesas = 5;
+if (n > 5) {
+    n = 5;
 }
 
-console.log(nome);
-console.log(renda);
-console.log(despesas);
+for (i = 0; i < n; i++) {
+    do {
+        despesa = Number(
+            prompt(`Digite o valor da ${i + 1}º despesa: `)
+        );
+    } while (isNaN(despesa));
+    soma += despesa;
+    sobra = renda - soma;
+}
+if (soma > renda) {
+    alert("⚠️ Atenção: você gastou mais do que ganhou.");
+}
+else if (sobra >= renda * 0.3) {
+    alert(`✅ Ótimo: você tem ${sobra} reais de sobra.`);
+}
+else {
+    alert("🙂 Ok: dá para melhorar a sobra.");
+}
+
+
+console.log(`Nome do usuário: ${nome}`);
+console.log(`Renda mensal do usuário: ${renda}`);
+console.log(`Número de despesas do usuário: ${despesa}`);
+console.log(`Total gasto em despesas pelo usuário: ${soma}`);
